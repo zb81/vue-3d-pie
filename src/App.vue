1a879c42a552237ac42544a09e1ae6299b2b25ac
朱备 <zbneversettle@gmail.com>
2023-06-08 14:21:45 +0800

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,8 @@
 <script setup lang="ts">
-// import Vue3dPie from './Vue3dPie'
-// import First from './study/First.vue'
-// import LightSource from './study/LightSource.vue'
-// import OrbitControls from './study/OrbitControls.vue'
-
-import Animation from './study/Animation.vue'
-import Array from './study/Array.vue'
-import Fullscreen from './study/Fullscreen.vue'
-import Geometry from './study/Geometry.vue'
-import Phong from './study/Phong.vue'
-import RendererConfig from './study/RendererConfig.vue'
-import Stats from './study/Stats.vue'
-
-// import PointLightHelper from './study/PointLightHelper.vue'
+import RendererConfig from './study/basic/RendererConfig.vue'
 </script>
 
 <template>
-  <!-- <Vue3dPie /> -->
-  <!-- <First /> -->
-  <!-- <LightSource /> -->
-  <!-- <PointLightHelper /> -->
-  <!-- <Animation /> -->
-  <!-- <Fullscreen /> -->
-  <!-- <Stats /> -->
-  <!-- <Array /> -->
-  <!-- <Geometry /> -->
-  <!-- <Phong /> -->
   <RendererConfig />
 </template>
 
